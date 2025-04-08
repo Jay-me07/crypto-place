50841dd299f37e6a28c0aceea13ebc1f9bb7aadd
Jay-me07 <jnjihiagachau07@gmail.com>
2025-04-08 12:38:46 +0300

    First commit

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+        <p>Copyright @ such is the way of wudan - All rights reserved</p>
+    </div>
+  )
+}
+
+export default Footer
